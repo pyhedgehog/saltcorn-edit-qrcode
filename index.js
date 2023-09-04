c4da81788c64e3a111150644d94b6d932bc23859
Michael Dubner <pywebmail@list.ru>
2023-09-04 05:37:22 +0300

    1.0.0

--- index.js
+++ index.js
@@ -8,17 +8,25 @@ const qrcode = {
     {
       name: "size",
       label: "Size",
+      type: 'Integer',
       sublabel: "Size of QR-code in pixels",
-      default: 15,
-      attributes: { max: 1000, min: 10 },
+      default: 50,
+      attributes: { max: 1000, min: 30 },
+    },
+    {
+      name: "border",
+      label: "White border",
+      type: 'Bool',
+      sublabel: "Add white border around QR-code",
+      default: false
     }
   ],
-  run: function(nm, v, attrs, cls) {
+  run: function(v, req, attrs) {
     var url = new URL('https://chart.googleapis.com/chart');
     url.searchParams.set('cht', 'qr');
-    url.searchParams.set('chs', `${attrs.size || 15}x${attrs.size || 15}`);
-    console.log(v);
-    url.searchParams.set('chl', `${typeof v}/${v.constructor.name} ${util.inspect(v)}`);
+    url.searchParams.set('chs', `${attrs.size || 150}x${attrs.size || 150}`);
+    url.searchParams.set('chl', String(v));
+    url.searchParams.set('chld', `L|${attrs.border?1:0}`);
     return img({src: url.toString()});
   }
 };
